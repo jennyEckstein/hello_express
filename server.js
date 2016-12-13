dc8d8828b08033633aad4bc64c55941cb0d9d3c6
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-12-12 22:17:40 -0500

    feat: more partials + helpers

--- server.js
+++ server.js
@@ -3,22 +3,29 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () =>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (request, response) => {
   // response.send('<h1>Hello Express!</h1>');
   response.render('home.hbs', {
-    pageTitle: 'About Page',
-    welcome: 'Hello World',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'Home Page',
+    welcome: 'Hello World'
   })
 });
 
 app.get('/about', (request, response) =>{
   response.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
